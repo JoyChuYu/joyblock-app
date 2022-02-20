7fa608020ec261f53403145f07baafa8f0988ba7
JoyChuYu <cc1804@live.mdx.ac.uk>
2022-02-21 03:58:22 +0800

    pagesize and map

--- backend/utils.js
+++ backend/utils.js
@@ -12,7 +12,7 @@ export const generateToken = (user) => {
     },
     process.env.JWT_SECRET || 'somethingsecret',
     {
-      expiresIn: '30d',
+      expiresIn: '300d',
     }
   );
 };
@@ -20,7 +20,7 @@ export const generateToken = (user) => {
 export const isAuth = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (authorization) {
-    const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
+    const token = authorization.slice(7, authorization.length); 
     jwt.verify(
       token,
       process.env.JWT_SECRET || 'somethingsecret',

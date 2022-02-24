1c111cc77be1cc765abae8cf7d2cc23908e86385
JoyChuYu <cc1804@live.mdx.ac.uk>
2022-02-24 21:09:29 +0800

    update mailgun

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -143,7 +143,7 @@ orderRouter.put(
           .messages()
           .send(
             {
-              from: 'Joys Block <JoysBlock@mg.yourdomain.com>',
+              from: 'Joys Block <JoysBlock@mg.sandbox2aee77f0a9f94996bea2d196b30bd819.mailgun.org>',
               to: `${order.user.name} <${order.user.email}>`,
               subject: `New order ${order._id}`,
               html: payOrderEmailTemplate(order),
